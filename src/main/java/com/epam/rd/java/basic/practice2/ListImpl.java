5f906602118d9d118d8b4c946b3a17b3e47b94c8
MadeControl <danya.polishchuk.2001@ukr.ent>
2020-07-08 20:03:20 +0300

    added Task2

--- src/main/java/com/epam/rd/java/basic/practice2/ListImpl.java
+++ src/main/java/com/epam/rd/java/basic/practice2/ListImpl.java
@@ -1,18 +1,30 @@
 package com.epam.rd.java.basic.practice2;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 
 public class ListImpl implements List {
 
+    private Node<Object> first;
+    private Node<Object> last;
+    private int size;
+
     @Override
-    public void clear() {
-        
+    public  void clear() {
+        for (Node<Object> x = first; x != null; ) {
+            Node<Object> next = x.next;
+            x.item = null;
+            x.next = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     public Iterator<Object> iterator() {
@@ -21,14 +33,23 @@ public class ListImpl implements List {
 
     private class IteratorImpl implements Iterator<Object> {
 
+        private int cursor = 0;
+
         @Override
         public boolean hasNext() {
-            return false;
+            return cursor != size;
         }
 
         @Override
         public Object next() {
-            return null;
+            if(cursor == 0 && size == 0){
+                throw new NoSuchElementException();
+            }
+            Node<Object> tempNode = first;
+            for(int i = 0; i < cursor; i++){
+                tempNode = tempNode.next;
+            } cursor++;
+            return tempNode.item;
         }
 
     }
@@ -45,50 +66,207 @@ public class ListImpl implements List {
 
     @Override
     public void addFirst(Object element) {
-        
+        linkFirst(element);
     }
 
     @Override
     public void addLast(Object element) {
-        
+        linkLast(element);
     }
 
     @Override
     public void removeFirst() {
-        
+        final Node<Object> f = first;
+        if (f == null){
+            throw new NoSuchElementException();
+        }
+        unlinkFirst(f);
     }
 
     @Override
     public void removeLast() {
-        
+        final Node<Object> l = last;
+        if (l == null){
+            throw new NoSuchElementException();
+        }
+        unlinkLast(l);
     }
 
     @Override
     public Object getFirst() {
-        return null;
+        final Node<Object> f = first;
+        if (f == null){
+            throw new NoSuchElementException();
+        }
+        return f.item;
     }
 
     @Override
     public Object getLast() {
-        return null;
+        final Node<Object> l = last;
+        if (l == null){
+            throw new NoSuchElementException();
+        }
+        return l.item;
     }
 
     @Override
     public Object search(Object element) {
+        Node<Object> temp = first;
+        for(Node<Object> x = first; x != null; x = x.next){
+            if(element.equals(x.item)){
+                return  x.item;
+            }
+        }
         return null;
     }
 
     @Override
     public boolean remove(Object element) {
+        if (element == null) {
+            for (Node<Object> x = first; x != null; x = x.next) {
+                if (x.item == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<Object> x = first; x != null; x = x.next) {
+                if (element.equals(x.item)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public String toString() {
-        return null;
+        Object[] array = this.toArray();
+        if (array == null)
+            return "null";
+
+        int iMax = array.length - 1;
+        if (iMax == -1)
+            return "[]";
+
+        StringBuilder b = new StringBuilder();
+        b.append('[');
+        for (int i = 0; ; i++) {
+            b.append(String.valueOf(array[i]));
+            if (i == iMax)
+                return b.append(']').toString();
+            b.append(", ");
+        }
     }
 
     public static void main(String[] args) {
 
+        ListImpl listImpl = new ListImpl();
+        listImpl.addFirst("First");
+        listImpl.addLast("something1");
+        listImpl.addLast("something2");
+        listImpl.addLast("something3");
+        listImpl.addLast("Last");
+        listImpl.size();
+        listImpl.getFirst();
+        listImpl.getLast();
+        listImpl.removeFirst();
+        listImpl.removeLast();
+        listImpl.search("something4");
+        listImpl.remove("something2");
+        listImpl.toString();
+        listImpl.clear();
+        listImpl.toString();
+
     }
+
+    private void linkFirst(Object e) {
+        final Node<Object> f = first;
+        final Node<Object> newNode = new Node<Object>(e, f);
+        first = newNode;
+        if (f == null){
+            last = newNode;
+        }
+//        else
+//            f.prev = newNode;
+        size++;
+    }
+
+    private void linkLast(Object e) {
+        final Node<Object> l = last;
+        final Node<Object> newNode = new Node<Object>(e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    private void unlinkFirst(Node<Object> f) {
+        final Node<Object> next = f.next;
+        f.item = null;
+        f.next = null; // help GC
+        first = next;
+        if (next == null){
+            last = null;
+        }
+        size--;
+    }
+
+    private void unlinkLast(Node<Object> l) {
+        final Node<Object> prev = findPrevNode(last.item);
+        l.item = null;
+//        l.prev = null; // help GC
+        last = prev;
+        if (prev == null)
+            first = null;
+        else
+            prev.next = null;
+        size--;
+    }
+
+    private Node<Object> findPrevNode(Object x){
+        Node<Object> curNode = first;
+        Node<Object> prevNode = null;
+        while (!x.equals(curNode)){
+            prevNode = curNode;
+            curNode = curNode.next;
+        } return  prevNode;
+    }
+
+    private void unlink(Node<Object> x) {
+        // assert x != null;
+        final Object element = x.item;
+        final Node<Object> next = x.next;
+        final Node<Object> prev = findPrevNode(x);
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+//            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+//            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+    }
+
+    private Object[] toArray() {
+        Object[] result = new Object[size];
+        int i = 0;
+        for (Node<Object> x = first; x != null; x = x.next)
+            result[i++] = x.item;
+        return result;
+    }
+
 }
--- src/test/java/com/epam/rd/java/basic/practice2/ListImplTest.java
+++ src/test/java/com/epam/rd/java/basic/practice2/ListImplTest.java
@@ -1,5 +1,153 @@
 package com.epam.rd.java.basic.practice2;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ListImplTest {
+
+    @Test
+    public void clearTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        list.clear();
+        Assert.assertEquals("[]", list.toString());
+
+    }
+
+    @Test
+    public void sizeTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        Assert.assertEquals(3, list.size());
+
+    }
+
+    @Test
+    public void addFirstTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addFirst("1");
+        list.addFirst("2");
+        list.addFirst("3");
+        Assert.assertEquals("[3, 2, 1]", list.toString());
+
+    }
+
+    @Test
+    public void addLastTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        Assert.assertEquals("[1, 2, 3]", list.toString());
+
+    }
+
+    @Test
+    public void removeFirstTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        list.removeFirst();
+        Assert.assertEquals("[2, 3]", list.toString());
+
+    }
+
+    @Test
+    public void removeLastTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        list.removeLast();
+        Assert.assertEquals("[1, 2]", list.toString());
+
+    }
+
+    @Test
+    public void removeExistElementTestTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        Assert.assertTrue(list.remove("3"));
+
+    }
+
+    @Test
+    public void removeNotExistElementTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        Assert.assertFalse(list.remove("4"));
+
+    }
+
+    @Test
+    public void getFirstTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        Assert.assertEquals("1", list.getFirst());
+
+    }
+
+    @Test
+    public void getLastTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        Assert.assertEquals("3", list.getLast());
+
+    }
+
+    @Test
+    public void searchExistElementTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        Assert.assertEquals("3", list.search("3"));
+
+    }
+
+    @Test
+    public void searchNotExistElementTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        Assert.assertNull(list.search("4"));
+
+    }
+
+    @Test
+    public void toStringTest(){
+
+        ListImpl list =  new ListImpl();
+        list.addLast("1");
+        list.addLast("2");
+        Assert.assertEquals("[1, 2]", list.toString());
+
+    }
 
 }
